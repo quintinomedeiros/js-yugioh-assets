1c798940235a4fe6c5a6144aa43c0bc9f3d08599
quintinomedeiros <quintinomedeiros@yaho.com.br>
2024-11-05 11:19:16 -0300

    jogo_yugioh_5.11.24

---
+++ src/scripts/engine.js
@@ -0,0 +1,176 @@
+// Declaração dos estados (manipulações)
+const state = {
+    score:{
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById('score_points'),
+    },
+    cardSprites: {
+        avatar: document.getElementById('card-image'),
+        name: document.getElementById('card-name'),
+        type: document.getElementById('card-type'),
+    },
+    playerSides: {
+        player1: "player-cards",
+        player1BOX: document.querySelector("#player-cards"),
+        computer: "computed-cards",
+        computerBOX: document.querySelector("#computed-cards"),
+    },
+    fieldCards: {
+        player: document.getElementById('player-field-card'),
+        computer: document.getElementById('computer-field-card'),
+    },
+    actions: {
+        button: document.getElementById('next-duel'),
+    },
+};
+
+// Mapeamento das cartas do jogo
+const pathImages = "./src/assets/icons/"
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        WinOf:[1],
+        LoseOf:[2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        WinOf:[2],
+        LoseOf:[0],
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        WinOf:[0],
+        LoseOf:[1],
+    },
+]
+
+// Selecionar pelo id uma carta aleatória
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+// 
+async function createCardImage(idCard, fieldSide) {
+    // Criar a carta virada no início do jogo
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", idCard);
+    cardImage.classList.add("card");
+
+    // Carta clicável somente do lado do jogador
+    if (fieldSide === state.playerSides.player1) {
+        // Mostrar a carta do jogador na esquerda quando passar o mouse em cima
+        cardImage.addEventListener("mouseover", ()=> {
+            drawSelectCard(idCard);
+        });
+
+        cardImage.addEventListener("click", ()=> {
+            // Pegar uma carta escolhida randomicamente quando clicar sobre a carta e mostrar no campo de jogo
+            setCardsField(cardImage.getAttribute("data-id"));
+        });
+    }
+
+    return cardImage;
+}
+
+// Função para remover todas as cartas do campo de duelo
+async function removeAllCardsImages() {
+    let { computerBOX, player1BOX } = state.playerSides; 
+    let imgElements = computerBOX.querySelectorAll("img");
+    imgElements.forEach((img) => img.remove());
+
+    imgElements = player1BOX.querySelectorAll("img");
+    imgElements.forEach((img) => img.remove());
+}
+
+// Função para colocar as cartas na área de disputa
+async function setCardsField(cardId) {
+    // Remover as outras cartas
+    await removeAllCardsImages();
+
+    // Seleção de uma carta aleatória para o computador
+    let computerCardId = await getRandomCardId();
+
+    // Mostrar o campo de disputa para os jogadores
+    state.fieldCards.player.style.display = "block";
+    state.fieldCards.computer.style.display = "block";
+
+    // Incluir imagem da carta no campo de disputa de cada jogador
+    state.fieldCards.player.src = cardData[cardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+
+    // Verificação do vencedor da rodada
+    let duelResults = await checkDuelResults(cardId, computerCardId);
+
+    await updateScore();
+    await drawButton(duelResults);
+}
+
+// Função para verificar o ganhador do duelo
+async function checkDuelResults(playerCardId, computerCardId) {
+    let duelResults = "Empate";
+    let playerCard = cardData[playerCardId];
+
+    if (playerCard.WinOf.includes(computerCardId)) {
+        duelResults = "Ganhou";
+        state.score.playerScore++;
+    }
+
+    if (playerCard.LoseOf.includes(computerCardId)) {
+        duelResults = "Perdeu";
+        state.score.computerScore++;
+    }
+
+    return duelResults;
+}
+
+// Ataulizar o placar do jogo
+async function updateScore() {
+    state.score.scoreBox.innerText = `Win: ${state.score.playerScore} ! Lose: ${state.score.computerScore}`;
+}
+
+// Criar o botão com resultado e reinício
+async function drawButton(text) {
+    state.actions.button.innerText = text;
+    state.actions.button.style.display = "block";
+}
+
+// Função para mostrar a carta do jogador na esquerda quando passa o mouse por cima
+async function drawSelectCard(index) {
+    state.cardSprites.avatar.src = cardData[index].img;
+    state.cardSprites.name.innerText = cardData[index].name;
+    state.cardSprites.type.innerText = "Atribute : " + cardData[index].type;
+}
+
+// Função para lançar cartas
+async function drawCards(cardNumbers, fieldSide) {
+    for (let i = 0; i < cardNumbers; i++) {
+        // Sorteio de uma carta aleatória
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+        
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
+// Função principal que chamará as outras funções
+function init() {
+    // Lançar cartas
+    drawCards(5, state.playerSides.player1);
+    drawCards(5, state.playerSides.computer);
+}
+
+// Inicialização do estado inicial do jogo
+init();
